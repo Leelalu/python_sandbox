ca0d81d0c09b288afa7297b4a8a789750a13240d
Leela <leelalu821@gmail.com>
2020-05-07 12:10:52 -0600

    adjust executable tag and adds a pomodero timer

---
+++ pomoderotimer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+
+import time
+import sys
+
+
+PTWORK = 0
+PTWAIT = 0
+PTCOUNT = 0
+
+try:
+    PTWORK = int(sys.argv[1])
+    PTWAIT = int(sys.argv[2])
+except (ValueError, IndexError):
+    print("Requriers work time and wait time in minutes as arguments")
+    sys.exit()
+
+#convert time to minutes
+PTWORK *= 60
+PTWAIT *= 60
+
+while True:
+    print("---TASKS---")
+    time.sleep(int(PTWORK))
+    print("---Good, now have some fun---")
+    time.sleep(int(PTWAIT))
+    PTCOUNT += 1
+    print("|||Pomodero cycles: ", PTCOUNT, "|||")
